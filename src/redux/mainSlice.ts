ffaa5c2ce961ec5881b614267da45da3edfdae1b
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-10-04 06:57:03 +0530

    redux

---
+++ src/redux/mainSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: any = {
+    booksList: [],
+    searchResults: {},
+};
+
+const bookSlice = createSlice({
+    name: 'books',
+    initialState,
+    reducers: {
+        setSearchResults: (state, action) => {
+            state.searchResults = state.list.filter(book =>
+              book.title.toLowerCase().includes(action.payload.toLowerCase())
+            );
+        },
+        updateUserInfo: (state, action) => {
+            state.userInformation = action.payload;
+        },
+    },
+});
+export const { setSearchResults, updateUserInfo } = bookSlice.actions;
+
+export default bookSlice.reducer; 
